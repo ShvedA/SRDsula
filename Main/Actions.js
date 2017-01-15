2e3aca5ff52781b7a0a829198c01d363b27216e3
ShvedA <alex.shved@mail.ru>
2017-01-16 00:54:56 +0200

    missing import

--- Main/Actions.js
+++ Main/Actions.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight  } from 'react-native';
 
 export default class Actions extends Component {
     render() {
